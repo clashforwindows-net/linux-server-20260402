39c2e7b632db6da351c3cb09bb08946c0f3334a4
clashforwindows-net <admin@clash-for-windows.net>
2026-04-02 20:10:06 +0800

    Add ssh-hardening.sh

---
+++ ssh-hardening.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sed -i 's/#Port 22/Port 2222/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+systemctl restart sshd
+echo "Done"
